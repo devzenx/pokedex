e96f195c57c4bde1a992ccd86a6e55ddb7f73e41
stephane <contact@depoilly.com>
2016-05-02 01:06:32 +0200

    PokeCell: Custom view cell for the UI Collection View added.

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Stéphane DEPOILLY on 02/05/2016.
+//  Copyright © 2016 Stéphane DEPOILLY. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    // We need an object to store pockemon data
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+
+        nameLbl.text = self.pokemon.name.capitalizedString // Since names in CSV are in lower case
+        thumbImg.image = UIImage (named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
