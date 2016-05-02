25d669833c298446e30fd05aab727b129d1b2f00
stephane <contact@depoilly.com>
2016-05-02 20:00:24 +0200

    IBOutlets created for DetailedViewVC Variables created in Pokemon.swift to update content in DetailedViewVC.

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -15,6 +15,15 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon! // Allows to transfer data from the main view controller
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,4 +33,7 @@ class PokemonDetailVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
